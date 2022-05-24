fd032d018ee327c18e9ea5d7fd62f3fe8cb9418e
TranDinhKhoi2002 <87485108+TranDinhKhoi2002@users.noreply.github.com>
2022-05-24 15:55:56 +0700

    Hoan thanh trang Home

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,3 +1,10 @@
+import { faMapLocation } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  HCMCityAddress,
+  southEastAddress,
+  southWestAddress,
+} from "../../data/address";
 import CollapseFooter from "./CollapseFooter";
 
 function Footer(props) {
@@ -12,7 +19,7 @@ function Footer(props) {
               className="h-[70px] mx-auto"
             />
           </div>
-          <div className="text-center mt-3">
+          <div className="text-center mt-3 px-[8%]">
             <p>Đặt hàng và thu tiền tận nơi toàn quốc</p>
             <h5 className="text-xl font-medium mb-2">(028) 7307 1441</h5>
             <a
@@ -23,6 +30,99 @@ function Footer(props) {
               Than phiền/Góp ý
             </a>
             <CollapseFooter />
+            <div className="grid md:grid-cols-3 grid-cols-1 mt-6">
+              <div className="">
+                <div>
+                  <h6 className="text-left text-base font-medium mb-4">
+                    <FontAwesomeIcon icon={faMapLocation} /> TÂY NAM BỘ
+                  </h6>
+                  <div>
+                    {southWestAddress.map((address, index) => (
+                      <p key={index} className="text-left mt-3 text-[13px]">
+                        <i
+                          className="w-4 h-4 inline-block"
+                          style={{
+                            backgroundImage:
+                              "url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QA/4ePzL8AAAAHdElNRQfiBQoDAgF3cpNwAAAA70lEQVQoz6XQzyqEcRTG8c95Z0j+RjTTWGBJuQBs2ShKuQzX4w7sLEZyBVNqlkoWNqMky5HRmBHvz8KMXrHzbE49fZ/OOU/4VoKq8Eh8u5mi5h3YNlO0fgJJSVPnF5CkSLMqqibtWlVNcykSiMHuDWsqOs7l9k1ou3ZJKCfYsuxExY4xJV2nnh0qaSSR2LSsrosV63JX7jHuUEujjJq2nmkdrWiRCFNePFkkEqP21OSOpTAAjox4UNfPgjcXbr0X2gnvbpzph4ygp6lbAHjV1ItfRf2h/wPlwfywYFeehrF5H8N7vx7LLJkoBLvu5IFPD8BHPpwYdgUAAAAldEVYdGRhdGU6Y3JlYXRlADIwMTgtMDUtMTBUMDM6MDI6MDEtMDQ6MDA8Zfw7AAAAJXRFWHRkYXRlOm1vZGlmeQAyMDE4LTA1LTEwVDAzOjAyOjAxLTA0OjAwTThEhwAAAABJRU5ErkJggg==')",
+                          }}
+                        />{" "}
+                        {address}
+                      </p>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div className="">
+                <div>
+                  <h6 className="text-left text-base font-medium mb-4">
+                    <FontAwesomeIcon icon={faMapLocation} /> ĐÔNG NAM BỘ VÀ TÂY
+                    NGUYÊN
+                  </h6>
+                  <div>
+                    {southEastAddress.map((address, index) => (
+                      <p key={index} className="text-left mt-3 text-[13px]">
+                        <i
+                          className="w-4 h-4 inline-block"
+                          style={{
+                            backgroundImage:
+                              "url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QA/4ePzL8AAAAHdElNRQfiBQoDAgF3cpNwAAAA70lEQVQoz6XQzyqEcRTG8c95Z0j+RjTTWGBJuQBs2ShKuQzX4w7sLEZyBVNqlkoWNqMky5HRmBHvz8KMXrHzbE49fZ/OOU/4VoKq8Eh8u5mi5h3YNlO0fgJJSVPnF5CkSLMqqibtWlVNcykSiMHuDWsqOs7l9k1ou3ZJKCfYsuxExY4xJV2nnh0qaSSR2LSsrosV63JX7jHuUEujjJq2nmkdrWiRCFNePFkkEqP21OSOpTAAjox4UNfPgjcXbr0X2gnvbpzph4ygp6lbAHjV1ItfRf2h/wPlwfywYFeehrF5H8N7vx7LLJkoBLvu5IFPD8BHPpwYdgUAAAAldEVYdGRhdGU6Y3JlYXRlADIwMTgtMDUtMTBUMDM6MDI6MDEtMDQ6MDA8Zfw7AAAAJXRFWHRkYXRlOm1vZGlmeQAyMDE4LTA1LTEwVDAzOjAyOjAxLTA0OjAwTThEhwAAAABJRU5ErkJggg==')",
+                          }}
+                        />{" "}
+                        {address}
+                      </p>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div className="">
+                <div>
+                  <h6 className="text-left text-base font-medium mb-4">
+                    <FontAwesomeIcon icon={faMapLocation} /> TP. HCM
+                  </h6>
+                  <div>
+                    {HCMCityAddress.map((address, index) => (
+                      <p key={index} className="text-left mt-3 text-[13px]">
+                        <i
+                          className="w-4 h-4 inline-block"
+                          style={{
+                            backgroundImage:
+                              "url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QA/4ePzL8AAAAHdElNRQfiBQoDAgF3cpNwAAAA70lEQVQoz6XQzyqEcRTG8c95Z0j+RjTTWGBJuQBs2ShKuQzX4w7sLEZyBVNqlkoWNqMky5HRmBHvz8KMXrHzbE49fZ/OOU/4VoKq8Eh8u5mi5h3YNlO0fgJJSVPnF5CkSLMqqibtWlVNcykSiMHuDWsqOs7l9k1ou3ZJKCfYsuxExY4xJV2nnh0qaSSR2LSsrosV63JX7jHuUEujjJq2nmkdrWiRCFNePFkkEqP21OSOpTAAjox4UNfPgjcXbr0X2gnvbpzph4ygp6lbAHjV1ItfRf2h/wPlwfywYFeehrF5H8N7vx7LLJkoBLvu5IFPD8BHPpwYdgUAAAAldEVYdGRhdGU6Y3JlYXRlADIwMTgtMDUtMTBUMDM6MDI6MDEtMDQ6MDA8Zfw7AAAAJXRFWHRkYXRlOm1vZGlmeQAyMDE4LTA1LTEwVDAzOjAyOjAxLTA0OjAwTThEhwAAAABJRU5ErkJggg==')",
+                          }}
+                        />{" "}
+                        {address}
+                      </p>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="md:flex items-center px-[8%] mt-6">
+            <div className="md:w-[80%]">
+              <p>© 2022 - CÔNG TY TNHH YAME VN</p>
+              <p className="text-[9px]">
+                Giấy CNĐKDN: 0310874914 – Ngày cấp: 25/11/2011 - Cơ quan cấp:
+                Phòng Đăng Ký Kinh Doanh – Sở Kế Hoạch và Đầu Tư TP.HCM
+              </p>
+              <p className="text-[9px]">
+                Địa chỉ đăng ký kinh doanh: 766/3B-3C Sư Vạn Hạnh (Nối dài),
+                Phường 12, Quận 10, TP.HCM - Điện thoại: (028) 3868 4857 - Mua
+                hàng: (028) 7307 1441 - Email: cskh@yame.vn
+              </p>
+            </div>
+            <div className="mt-3">
+              <a
+                href="http://online.gov.vn/Home/WebDetails/1840"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  className="w-full max-w-[150px]"
+                  alt=""
+                  src="https://res.yame.vn/dathongbao.png"
+                />
+              </a>
+            </div>
           </div>
         </div>
       </div>
